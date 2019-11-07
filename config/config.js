3cc876336ea2cea438521a8740494ec73a6d2a36
Nicholas Hoffman <njhoffman1982@gmail.com>
2019-11-07 15:43:31 -0600

    Output scraped song data to output files for manual editing

--- config/config.js
+++ config/config.js
@@ -6,37 +6,39 @@ module.exports = () => ({
     colors: {
       fatal: { fg: [255, 255, 255], bg: [230, 20, 20] },
       error: { fg: [255, 255, 255], bg: [180, 20, 20] },
-      warn:  { fg: [255, 255, 255], bg: [180, 60, 20] },
-      log:   { fg: [255, 255, 255], bg: [60, 110, 180] },
-      info:  { fg: [255, 255, 255], bg: [20, 70, 180] },
+      warn: { fg: [255, 255, 255], bg: [180, 60, 20] },
+      log: { fg: [255, 255, 255], bg: [60, 110, 180] },
+      info: { fg: [255, 255, 255], bg: [20, 70, 180] },
       debug: { fg: [255, 255, 255], bg: [80, 100, 200] },
-      trace: { fg: [255, 255, 255], bg: [80, 120, 220] },
+      trace: { fg: [255, 255, 255], bg: [80, 120, 220] }
     },
     file: {
       name: 'youtube-mp3.log',
       level: 3
     },
     stdout: {
-      level: 5
+      level: 4
     }
   },
   data: {
     plugDj: {
-      filePath: path.join(`${appRoot}`, 'data/plug.dj.json'),
+      filePath: path.join(`${appRoot}`, 'data/03-24-2019/plug.dj.json'),
+      outputPath: path.join(`${appRoot}`, 'output/plug.dj.tsv'),
       ignored: []
     },
     dubtrackFm: {
-      filePath: path.join(`${appRoot}`, 'data/dubtrack.fm.json'),
-      ignored: []
-    },
-    youtube: {
-      filePath: path.join(`${appRoot}`, 'data/youtube.json'),
+      filePath: path.join(`${appRoot}`, 'data/03-24-2019/dubtrack.fm.json'),
+      outputPath: path.join(`${appRoot}`, 'output/dubtrack.fm.tsv'),
       ignored: []
     }
+    // youtube: {
+    //   filePath: path.join(`${appRoot}`, 'data/youtube.json'),
+    //   ignored: []
+    // }
   },
-  ffmpegPath:          '/path/to/ffmpeg', // Where is the FFmpeg binary located?
-  outputPath:          '/path/to/mp3/folder', // Where should the downloaded and encoded files be stored?
+  ffmpegPath: '/path/to/ffmpeg', // Where is the FFmpeg binary located?
+  outputPath: '/path/to/mp3/folder', // Where should the downloaded and encoded files be stored?
   youtubeVideoQuality: 'highest', // What video quality should be used?
-  queueParallelism:    2, // How many parallel downloads/encodes should be started?
-  progressTimeout:     2000 // How long should be the interval of the progress reports
+  queueParallelism: 2, // How many parallel downloads/encodes should be started?
+  progressTimeout: 2000 // How long should be the interval of the progress reports
 });
